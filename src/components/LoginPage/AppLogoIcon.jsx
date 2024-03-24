33af1d139b624931f8d1938628779061de89cecc
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2024-03-23 20:42:22 -0400

    "Add app logo in SignInSide component"
    The avatar component is updated to display the app logo using an image source, and the text fields and checkbox for remembering the user are removed.

---
+++ src/components/LoginPage/AppLogoIcon.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import AppLogo from "../assets/AppLogo.png";
+
+AppLogoIcon = (props) => {
+  return <img src={AppLogo} alt="App Logo" {...props} />;
+};
--- src/components/LoginPage/SignInSide.jsx
+++ src/components/LoginPage/SignInSide.jsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -12,6 +9,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import appLogo from "../../assets/AppLogo.png";
 
 function Copyright(props) {
   return (
@@ -78,7 +76,16 @@ export default function SignInSide() {
           >
             <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               {/* <LockOutlinedIcon /> */}
+              <img
+                src={appLogo}
+                alt="App Logo"
+                style={{ width: "100%", height: "auto" }}
+              />
             </Avatar>
+            {/* <img src="../../assets/AppLogo.png" alt="App Logo" /> */}
+            {/* <AppLogoIcon style={{ width: "25px", height: "auto" }} /> */}
+            {/* <Typography level="title-lg">PlayItForward</Typography> */}
+
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
@@ -88,30 +95,6 @@ export default function SignInSide() {
               onSubmit={handleSubmit}
               sx={{ mt: 1 }}
             >
-              {/* <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              /> */}
-              {/* <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              /> */}
-              {/* <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              /> */}
               <Button
                 type="submit"
                 fullWidth
@@ -120,18 +103,6 @@ export default function SignInSide() {
               >
                 Sign In
               </Button>
-              {/* <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid> */}
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
